7bf734ab9acd81e3d9b03a2756a96cbc198afb9f
unknown <SunilLimje1979>
2024-02-15 10:25:37 +0530

    setings

--- masters/settings.py
+++ masters/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0&m0s98+rzat(9&yxf!75!&g-txt+car_2ibhbx+xsq186w6o#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","localhost","localhost:5432","0.0.0.0:8000","13.233.211.102","13.233.211.102:8000"]
 
 
 # Application definition
@@ -77,15 +77,12 @@ WSGI_APPLICATION = 'masters.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db_gyaagl_vocabuli_medicify',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'USER': 'postgres',
+        'PASSWORD': 'Medicify@2024',
+        'HOST': '13.233.211.102',
+        'PORT': '5432',
     }
 }
 
