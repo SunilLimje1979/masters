3182566f6e402aac8762ad596698f939cac8430f
unknown <SunilLimje1979>
2024-02-15 11:45:40 +0530

    setings

--- masters/settings.py
+++ masters/settings.py
@@ -118,11 +118,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+STATIC_URL = '/masters/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "masters/static",
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
