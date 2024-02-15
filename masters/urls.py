df05e7030c3fbaf1c923cefb0e1bfd74fe124b6d
unknown <SunilLimje1979>
2024-02-15 11:49:12 +0530

    setings

--- masters/urls.py
+++ masters/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('masters/admin/', admin.site.urls),
 
-    path("api/",include("mastersapp.urls")),
+    path("masters/api/",include("mastersapp.urls")),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
